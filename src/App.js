3a320553ea5e6607776da380729376ade8b62499
niranjan kumar <niranjankumaraina@gmail.com>
2023-03-17 16:03:50 +0530

    17-03-2023

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import { useState } from "react";
 import "./App.css";
+import Alert from "./components/Alert";
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 function App() {
   const [mode, setmode] = useState("light"); // This will show dark mode is enable or not.
+  const [alert, setalert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setalert({ msg: message, type: type });
+  };
 
   const toggleMode = () => {
     if (mode === "light") {
       setmode("dark");
       document.body.style.background = "#000d50";
+      showAlert("Dark mode has been enabled.", "succes : ");
     } else {
       setmode("light");
       document.body.style.background = "white";
+      showAlert("Light mode has been enabled.", "succes : ");
     }
   };
 
@@ -24,7 +32,7 @@ function App() {
         mode={mode}
         toggleMode={toggleMode}
       />
-      {/* <Navbar tittle="textUtils" /> */}
+      <Alert alert={alert} />
       <div className="container my-3">
         <Textform heading="Enter the text for analyze below" mode={mode} />
         {/* <About/> */}
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
-export default function Alert() {
+export default function Alert(props) {
   return (
+    props.alert &&
     <div>
-      
+      <div
+        className="alert alert-warning alert-dismissible fade show"
+        role="alert"
+      >
+        <strong>{props.alert.type}</strong>{props.alert.msg}
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        ></button>
+      </div>
     </div>
-  )
+  );
 }
